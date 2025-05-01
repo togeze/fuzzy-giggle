062a97c2277f486ef9484cd987f2e76275639ebd
Ruslaniys47 <if_ff@mail.com>
2025-05-02 00:37:16 +0300

    feat: added config

--- bot/main.py
+++ bot/main.py
@@ -1,17 +1,9 @@
 import asyncio
 import os
-
-from dotenv import find_dotenv, load_dotenv
 from aiogram import Bot, Dispatcher, html, types
 
-from aiogram.filters import CommandStart, Command
-from aiogram.types import Message
-from aiogram import F
-
-load_dotenv(find_dotenv())
-
+from settings.config import TOKEN, ADMINS
 from settings.commands import user_cmds, admin_cmds
-from keyboard.factory import KeyboardFactory
 from core.middlewares.role_checker import RoleMiddleware
 from features.admin.presentation.routers import AdminRouter
 from features.user.presentation.routers import UserRouter
@@ -19,33 +11,21 @@ from features.user.presentation.routers import UserRouter
 
 ALLOWED_UPDATES = ['message, edited_message']
 
-TOKEN = os.getenv('TOKEN')
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
-
-# @dp.message(F.text, Command("exit"))
-# async def any_message(message: Message):
-#     await message.answer(
-#         "Пока, до новых встреч!"  # TODO сбросить всё в ноль, убрать все кнопки
-#     )
-
-# добавить /settings где будет краткая инфа о боте
-
-# @dp.message(CommandStart())
-# async def command_start_handler(message: Message) -> None:
-#     await message.answer(f"Приветствую, {html.bold(message.from_user.full_name)}!", reply_markup=KeyboardFactory.get_main_menu())
-#     await message.answer("Example", reply_markup=KeyboardFactory.get_example_inline_menu())
 
 
 async def main():
     await bot.delete_webhook(drop_pending_updates=True)
     await bot.delete_my_commands(scope=types.BotCommandScopeAllPrivateChats())
     await bot.set_my_commands(commands=user_cmds, scope=types.BotCommandScopeAllPrivateChats())
-    await bot.set_my_commands(
-        commands=admin_cmds,
-        scope=types.BotCommandScopeChat(chat_id=1870995730)
-    )
+
+    for admin_id in ADMINS:
+        await bot.set_my_commands(
+            commands=admin_cmds,
+            scope=types.BotCommandScopeChat(chat_id=admin_id)
+        )
 
     dp.update.middleware(RoleMiddleware())
 
